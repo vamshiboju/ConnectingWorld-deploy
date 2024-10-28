ab8fdb5564023af2951275e9db1b8e24a5128ed3
vamshiboju <83817973+vamshiboju@users.noreply.github.com>
2024-10-28 12:03:00 -0500

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -18,12 +18,12 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(function (req, res, next) {
-  //  res.setHeader(
-  //    "Access-Control-Allow-Origin",
-  //    "https://connectingworld.onrender.com"
-  // );
+  res.setHeader(
+     "Access-Control-Allow-Origin",
+     "https://connectingworld-front-end.onrender.com",
+  );
   //test
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
   // Request methods you wish to allow
   res.setHeader(
